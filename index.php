0cbb550f28f3411b1930e2076260b182728e5e16
Raghu-Cloudgeeks <50106396+Raghu-Cloudgeeks@users.noreply.github.com>
2019-09-30 17:43:21 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<title> CLOUDGEEKS CLASS OF ELB/ASG DEMO </title>
+<body>
+<h1>
+<center>
+<IMG SRC="EIC.jpg" ALT="EIC"><br>
+<?php
+echo $_SERVER['SERVER_ADDR'];
+?>
+</center>
+</h1>
+</body>
+</html>
