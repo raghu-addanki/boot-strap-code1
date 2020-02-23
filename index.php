60b3d168e97942ae707b8f39b22182ae84ffb01c
CGT <50106396+Raghu-Cloudgeeks@users.noreply.github.com>
2020-02-23 09:14:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,26 @@
 <IMG SRC="clogo.png" ALT="clogo"><br>
 <?php
 echo $_SERVER['SERVER_ADDR'];
+  // PHP code to extract IP  
+  
+function getVisIpAddr() { 
+      
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) { 
+        return $_SERVER['HTTP_CLIENT_IP']; 
+    } 
+    else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { 
+        return $_SERVER['HTTP_X_FORWARDED_FOR']; 
+    } 
+    else { 
+        return $_SERVER['REMOTE_ADDR']; 
+    } 
+} 
+  
+// Store the IP address 
+$vis_ip = getVisIPAddr(); 
+  
+// Display the IP address 
+echo $vis_ip; 
 ?>
 </center>
 </h1>
