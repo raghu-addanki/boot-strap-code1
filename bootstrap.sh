64edaad58527026406c637d8b065dd6f2fdfb140
raghu-addanki <70473573+raghu-addanki@users.noreply.github.com>
2020-08-30 18:00:37 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 yum install -y httpd php git
 service httpd start
 cd
-git clone https://github.com/Raghu-Cloudgeeks/elb2019.git
-cp ./elb2019/* /var/www/html
+git clone https://github.com/raghu-addanki/boot-strap-code1
+cp ./boot-strap-code1/* /var/www/html
 mv /var/www/html/htaccess /var/www/html/.htaccess
